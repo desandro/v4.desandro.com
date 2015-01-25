73ee7ddf6ad9ab196f18ba647f0d78a9a1b15bc0
David DeSandro <desandrocodes@gmail.com>
2015-01-25 09:18:14 -0500

    initial commit with header

---
+++ gulpfile.js
@@ -0,0 +1,57 @@
+/*jshint node: true */
+
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+// var replace = require('gulp-replace');
+// var uglify = require('gulp-uglify');
+
+// ----- content ----- //
+
+var contentSrc = [
+  'content/head.mustache'
+];
+
+gulp.task( 'content', function() {
+  gulp.src( contentSrc )
+    .pipe( concat('index.html') )
+    .pipe( gulp.dest('build') );
+});
+
+// ----- css ----- //
+
+var cssSrc = [
+  'bower_components/normalize.css/normalize.css',
+  'css/web-fonts.css',
+  'css/base.css',
+  'css/header.css'
+];
+
+gulp.task( 'css', function() {
+  gulp.src( cssSrc )
+    .pipe( concat('styles.css') )
+    .pipe( gulp.dest('build/css') );
+});
+
+// ----- assets ----- //
+
+// copy assets to build/
+
+gulp.task( 'fonts', function() {
+  gulp.src( 'fonts/*.*' )
+    .pipe( gulp.dest('build/fonts') );
+});
+
+gulp.task( 'img', function() {
+  gulp.src( 'img/*.*' )
+    .pipe( gulp.dest('build/img') );
+});
+
+gulp.task( 'assets', [ 'fonts', 'img' ]);
+
+// ----- default ----- //
+
+gulp.task( 'default', [
+  'content',
+  'css',
+  'assets'
+] );
